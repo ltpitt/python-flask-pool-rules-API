20e479a8cd8fff78b7428aa256b1eb84d37a8d24
Davide Nastri <d.nastri@gmail.com>
2017-03-18 11:46:11 +0100

    1st commit from Python Item Catalog

---
+++ db_populate.py
@@ -0,0 +1,84 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, User, Category, Item
+
+app = Flask(__name__)
+
+engine = create_engine('postgresql://catalog:VZgDYgVZXrwVSwaNWTC0@localhost:5432/catalog')
+#engine = create_engine('sqlite:///item-catalog.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+newCategory = Category(
+    name="Category 1",
+    description="Description Category 1"
+)
+
+session.add(newCategory)
+session.commit()
+
+newCategory = Category(
+    name="Category 2",
+    description="Description Category 2"
+)
+
+session.add(newCategory)
+session.commit()
+
+newItem = Item(
+    name="Item 1",
+    description="Description Item 1",
+    price="10",
+    image="/static/images/test.jpg",
+    category_id=1
+)
+
+session.add(newItem)
+session.commit()
+
+newItem = Item(
+    name="Item 2",
+    description="Description Item 2",
+    price="20",
+    image="/static/images/test.jpg",
+    category_id=2
+)
+
+session.add(newItem)
+session.commit()
+
+newItem = Item(
+    name="Item 3",
+    description="Description Item 3",
+    price="30",
+    image="/static/images/test.jpg",
+    category_id=1
+)
+
+session.add(newItem)
+session.commit()
+
+newItem = Item(
+    name="Item 4",
+    description="Description Item 4",
+    price="40",
+    image="/static/images/test.jpg",
+    category_id=1
+)
+
+session.add(newItem)
+session.commit()
+
+newItem = Item(
+    name="Item 5",
+    description="Description Item 5",
+    price="50",
+    image="/static/images/test.jpg",
+    category_id=2
+)
+
+session.add(newItem)
+session.commit()
---
+++ db_setup.py
@@ -0,0 +1,118 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+
+from sqlalchemy.dialects.sqlite import BLOB
+
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy.orm import relationship
+
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(
+        Integer, primary_key=True
+    )
+    name = Column(
+        String(250), nullable=False
+    )
+    email = Column(
+        String(250), nullable=False
+    )
+    picture = Column(
+        String(250)
+    )
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email,
+            'picture': self.picture,
+        }
+
+
+class Category(Base):
+    __tablename__ = 'category'
+    id = Column(
+        Integer, primary_key=True
+    )
+
+    name = Column(
+        String(250), nullable=False
+    )
+
+    description = Column(
+        String(250), nullable=False
+    )
+
+    user_id = Column(
+        Integer, ForeignKey('user.id')
+    )
+    user = relationship(User)
+
+    item = relationship("Item",
+                        cascade="all, delete-orphan",
+                        passive_deletes=True)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'user_id': self.user_id,
+        }
+
+
+class Item(Base):
+    __tablename__ = 'item'
+    id = Column(
+        Integer, primary_key=True
+    )
+    name = Column(
+        String(250), nullable=False
+    )
+    description = Column(
+        String(250), nullable=False
+    )
+    image = Column(
+        String(250)
+    )
+    price = Column(
+        String(250), nullable=False
+    )
+
+    category_id = Column(
+        Integer, ForeignKey('category.id', ondelete='CASCADE'), nullable=False
+    )
+    category = relationship("Category")
+
+    user_id = Column(
+        Integer, ForeignKey('user.id')
+    )
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'image': self.image,
+            'price': self.price,
+            'category_id': self.category_id,
+            'user_id': self.user_id,
+        }
+
+
+# SQLite engine
+# engine = create_engine('sqlite:///item-catalog.db')
+# PostgreSQL engine
+engine = create_engine('postgresql://catalog:VZgDYgVZXrwVSwaNWTC0@localhost:5432/catalog')
+
+Base.metadata.create_all(engine)
